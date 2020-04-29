90e8fa7d7de2865934e77ee5cdd97a316d006bbc
me <me@me.me.com>
2020-04-29 15:48:34 -0700

    implement the fifth spec

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,22 @@
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var vowels = ["a","e","i","o","u"];
 function pigLatin(input){
- if (!alphabet.includes(input[0])){
-   return input;
- }
- if(vowels.includes(input[0])){
-   return input + "way";
- }
- if(!vowels.includes(input[0])){
-   var firstLetter = input[0];
-   var rest = input.slice(1);
-   return rest + firstLetter + "ay"  
- }
  
+  var array = input.split(" ").map(item => {
+  if (!alphabet.includes(item[0])){
+    return item;
+  }
+  if(vowels.includes(item[0])){
+    return item + "way";
+  }
+  if(!vowels.includes(item[0])){
+    var firstLetter = item[0];
+    var rest = item.slice(1);
+    return rest + firstLetter + "ay"  
+  }
+ });
+ 
+ return array.join(" ");
 }
 
 $(document).ready(function(){
