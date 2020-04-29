f60a0091af32aa27cd807921fdab2cf254ffa25d
me <me@me.me.com>
2020-04-29 15:31:24 -0700

    implement the 3rd spec

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var vowels = ["a","e","i","o","u"];
 function pigLatin(input){
- if (!alphabet.includes(input)){
+ if (!alphabet.includes(input[0])){
    return input;
  }
- if(vowels.includes(input)){
+ if(vowels.includes(input[0])){
    return input + "way";
  }
 }
