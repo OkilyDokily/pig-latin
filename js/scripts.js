31585039154c4f7f1ba6b5d1fbd108e8f01e9c0c
me <me@me.me.com>
2020-04-30 13:57:38 -0700

    finished final spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var vowels = ["a","e","i","o","u"];
+alphabet = alphabet.concat(alphabet.map(item => item.toUpperCase()));
+var vowels = ["a","e","i","o","u","A","E","I","O","U"];
 function pigLatin(input){
 
   var array = input.split(" ").map(item => {
