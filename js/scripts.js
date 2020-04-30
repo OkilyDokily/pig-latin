516c3762c4e5812755fd68ea4965480613c2ee40
me <me@me.me.com>
2020-04-30 13:42:55 -0700

    implement the 7th spec

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,6 @@
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var vowels = ["a","e","i","o","u"];
 function pigLatin(input){
-  if(input.split(" ").length === 1 && !vowels.includes(input[0]) && alphabet.includes(input[0])){
-    var consonants = [];
-    consonants.push(input[0]);
-    for(var i = 1; i < input.length;i++){
-      if(!vowels.includes(input[i])){
-        consonants.push(input[i]);
-      }
-      else{
-        break;
-      }
-    }
-
-    if(consonants.join("") === "q" && input[1]==='u'){
-      consonants.push(['u']);
-    }
-    
-    if(consonants.join("") === "sq" && input[2]==='u'){
-      consonants.push(['u']);
-    }
-    
-    var rest =  input.slice(consonants.length);
-    return rest + consonants.join("") + "ay";  
-  }
 
   var array = input.split(" ").map(item => {
     if (!alphabet.includes(item[0])){
@@ -34,9 +11,28 @@ function pigLatin(input){
       return item + "way";
     }
     if(!vowels.includes(item[0])){
-      var firstLetter = item[0];
-      var rest = item.slice(1);
-      return rest + firstLetter + "ay"  
+      
+      var consonants = [];
+      consonants.push(item[0]);
+      for(var i = 1; i < item.length;i++){
+        if(!vowels.includes(item[i])){
+          consonants.push(item[i]);
+        }
+        else{
+          break;
+        }
+      }
+  
+      if(consonants.join("") === "q" && item[1]==='u'){
+        consonants.push(['u']);
+      }
+      
+      if(consonants.join("") === "sq" && item[2]==='u'){
+        consonants.push(['u']);
+      }
+      
+      var rest =  item.slice(consonants.length);
+      return rest + consonants.join("") + "ay";  
     }
  });
  
